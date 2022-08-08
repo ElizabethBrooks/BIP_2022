79011004bb4180c34cb2a841f50078af93d28f90
Elizabeth Brooks <merimaat1@gmail.com>
2022-08-08 15:34:12 -0400

    Update BIP_2022_notes.sh

--- scripts/BIP_2022_notes.sh
+++ scripts/BIP_2022_notes.sh
@@ -1,12 +1,7 @@
 # BIP 2022 notes
 
 # intro to the shell setup
-mkdir ~/BIP_2022
-mkdir ~/BIP_2022/var
-mkdir ~/BIP_2022/usr
-mkdir ~/BIP_2022/usr/lib
-mkdir ~/BIP_2022/home
-mkdir ~/BIP_2022/home/username
+cd
 
 # if necessary, install tree
 sudo apt install tree
@@ -18,10 +13,9 @@ sudo apt update
 sudo apt install tree
 
 # check directory structure
-tree ~/BIP_2022
+tree
 
 # import data
-cd ~/BIP_2022/home/username
 git clone https://github.com/tracykteal/msu_al340.git
 
 # check updated directory structure
